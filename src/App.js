5b9bdb5871ed90997f19c7037e56ae8829b6d50c
Folareen <sakawahab03@gmail.com>
2022-06-16 13:46:00 +0100

    added loader

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@
 import './css/index.css';
 import Main from './Main';
 import Highlights from './Highlights';
+import Loading from './Loading';
 import {FaSearch} from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 
 export function displayDate(day){
   const DAYS = [
@@ -17,46 +18,32 @@ export function displayDate(day){
   const currDay = new Date().getDay();
   const actualDay = (currDay + day) > 6 ? (currDay + day) -7 : currDay + day
   const month = new Date().getMonth();
-
-  // console.log(actualDay);
-  // console.log(currDay);
-  console.log(day);
-
+  
   return `${DAYS[actualDay]}, ${currDate + day} ${MONTH[month]}`
 }
 
 
 function App() {
 
-  let currentLocation = "Lagos";
-
-  // async function getLocation(){
-  //   if (navigator.geolocation) {
-  //       navigator.geolocation.getCurrentPosition(function(position){
-  //         fetch(`http://api.positionstack.com/v1/reverse?access_key=93b6e5f543ee6a01143560cfa87c32b6&query=${position.coords.latitude},${position.coords.longitude}`)
-  //           .then(data => data.json() )
-  //           .then(res => currentLocation = res.data[0].region )
-  //           .then(console.log("done"))
-  //       })
-  //   }
-  //   else{
-  //     alert("Pls, allow location")
-  //   }
-  // }
-
-  const [query, setQuery] = useState(currentLocation);
+  const [query, setQuery] = useState("Lagos");
   const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(true)
 
   let weatherData = [];
 
-  function searchWeather () {
+    useEffect(
+      () => {
+        searchWeather()
+      }, []
+    )
 
-    (async () => {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&units=metric&appid=b8b011d30d5cf438733113076fb03920`);
-      const res = await response.json();
-      setWeather(res);
+  async function searchWeather () {
+    setLoading(true)
 
-    })()
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&units=metric&appid=b8b011d30d5cf438733113076fb03920`);
+    const res = await response.json();
+    setWeather(res);
+    setLoading(false)
 
     document.querySelector('.main--search').classList.remove('open-search')
 
@@ -76,6 +63,13 @@ function App() {
   }
 
   return (
+
+    loading ?
+    (
+      <Loading/>
+    )
+    :
+    (
     <>  
       {window.addEventListener('load', () => {
         searchWeather()
@@ -96,6 +90,7 @@ function App() {
       <Main temp = {isNotEmpty() ? Math.ceil(weatherData[0]["main"]["temp"])  : ""} condition = {isNotEmpty() ? weatherData[0]["weather"][0]["description"] : ""} location = {query} />
       <Highlights  nextData = {weatherData} weatherHighlightData = {weatherData[0]} />
     </>
+    )
   );
 }
 
---
+++ src/Loading.jsx
@@ -0,0 +1,15 @@
+import logo from './images/logo.svg';
+
+export default function (){
+    return (
+        <div  className='loading'>
+            <img src={logo} alt="react" className="react-logo" />
+            <h2>
+                Loading...
+            </h2>
+        </div>
+
+    )
+
+
+}
